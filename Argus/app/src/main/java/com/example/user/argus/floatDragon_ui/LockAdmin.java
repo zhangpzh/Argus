d5497f7063fe03c306f4412ff5514e783e710a66
Gaven Zhang <zhangpzh666@github.com>
2015-09-15 21:43:11 +0800

    大修主界面

--- Argus/app/src/main/java/com/example/user/argus/floatDragon_ui/LockAdmin.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.argus.floatDragon_ui;
-
-import android.app.admin.DeviceAdminReceiver;
-
-/**
- * Created by zouyun on 15/9/10.
- */
-public class LockAdmin extends DeviceAdminReceiver {
-}
